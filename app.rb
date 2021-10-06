42faedeeba24af423a23a68cf1fe69b5eec30f06
Cameron Koegel <53310569+ckoegel@users.noreply.github.com>
2021-10-06 09:14:34 -0400

    DX-2240 Updated Sample App and README.md (#2)

--- app.rb
+++ app.rb
@@ -6,85 +6,61 @@ require 'bandwidth'
 include Bandwidth
 include Bandwidth::Messaging
 
+BW_USERNAME = ENV.fetch("BW_USERNAME")
+BW_PASSWORD = ENV.fetch("BW_PASSWORD")
+BW_NUMBER = ENV.fetch("BW_NUMBER")
+BW_MESSAGING_APPLICATION_ID = ENV.fetch("BW_MESSAGING_APPLICATION_ID")
+BW_ACCOUNT_ID = ENV.fetch("BW_ACCOUNT_ID")
+LOCAL_PORT = ENV.fetch("LOCAL_PORT")
+
+set :port, LOCAL_PORT
+
 bandwidth_client = Bandwidth::Client.new(
-    messaging_basic_auth_user_name: "username",
-    messaging_basic_auth_password: "password"
+    messaging_basic_auth_user_name: BW_USERNAME,
+    messaging_basic_auth_password: BW_PASSWORD
 )
 messaging_client = bandwidth_client.messaging_client.client
 
-account_id = "id"
+account_id = BW_ACCOUNT_ID
 
-post '/outboundMessage' do
+post '/callbacks/outbound/messaging' do
     #Make a POST request to this URL to send a text message
+    data = JSON.parse(request.body.read)
     body = MessageRequest.new
-    body.application_id = "id"
-    body.to = ["+1to"]
-    body.from = "+1from"
-    body.text = "Hello from Bandwidth"
+    body.application_id = BW_MESSAGING_APPLICATION_ID
+    body.to = [data["to"]]
+    body.from = BW_NUMBER
+    body.text = data["text"]
 
-    messaging_client.create_message(account_id, :body => body)
+    messaging_client.create_message(account_id, body)
 
     return ''
 end
 
-post '/messageCallback' do
-    #This URL handles callbacks from Bandwidth. All messaging callbacks (inbound and outbound)
-    #will come to this URL. The message type must be checked to know if it's an inbound or
-    #outbound message.
+post '/callbacks/outbound/messaging/status' do
+    data = JSON.parse(request.body.read)
+    case data[0]["type"] 
+        when "message-sending"
+            puts "message-sending type is only for MMS"
+        when "message-delivered"
+            puts "your message has been handed off to the Bandwidth's MMSC network, but has not been confirmed at the downstream carrier"
+        when "message-failed"
+            puts "For MMS and Group Messages, you will only receive this callback if you have enabled delivery receipts on MMS."
+        else
+            puts "Message type does not match endpoint. This endpoint is used for message status callbacks only."
+        end
+    return ''
+end
+
+post '/callbacks/inbound/messaging' do
+    #This URL handles inbound messages.
     #If the inbound message contains media, that media is downloaded
     data = JSON.parse(request.body.read)
     if data[0]["type"] == "message-received"
         puts "Message received"
-        puts data
-        if data[0]["message"].key?("media")
-            data[0]["message"]["media"].each do |media|
-                media_id = media.split("/").last(3)
-                downloaded_media = messaging_client.get_media(account_id, media_id).data
-                puts downloaded_media
-            end
-        end
+        puts "To: " + data[0]["message"]["to"][0] + "\nFrom: " + data[0]["message"]["from"] + "\nText: " + data[0]["message"]["text"]
     else
-        puts data
+        puts "Message type does not match endpoint. This endpoint is used for inbound messages only.\nOutbound message callbacks should be sent to /callbacks/outbound/messaging."
     end
-
-    return ''
-end
-
-post '/mediaManagement' do
-    #Make a POST request to this endpoint to upload a media file to Bandwidth, then download it
-    #and print its contents
-    media = "simple text string"
-    media_id = "bandwidth-sample-app"
-
-    messaging_client.upload_media(account_id, media_id, media.length.to_s, media, :content_type => "application/octet-stream", :cache_control => "no-cache")
-
-    downloaded_media = messaging_client.get_media(account_id, media_id).data
-    puts downloaded_media
-
-    return ''
-end
-
-post '/outboundMediaMessage' do
-    #Make a post request to this url to send outbound MMS with media
-
-    #Media previously uploaded to bandwidth
-    body = MessageRequest.new
-    body.application_id = "id"
-    body.to = ["+1to"]
-    body.from = "+1from"
-    body.text = "Hello from Bandwidth"
-    body.media = ["https://messaging.bandwidth.com/api/v2/users/%s/media/bandwidth-sample-app" % [account_id]]
-
-    messaging_client.create_message(account_id, :body => body)
-
-    #Media not on bandwidth
-    body = MessageRequest.new
-    body.application_id = "id"
-    body.to = ["+1to"]
-    body.from = "+1from"
-    body.text = "Hello from Bandwidth"
-    body.media = ["https://assets1.ignimgs.com/2019/09/04/super-mario-world---button-fin-1567640652381.jpg"]
-
-    messaging_client.create_message(account_id, :body => body)
     return ''
 end
